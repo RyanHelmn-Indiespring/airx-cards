aaa287e081196aded076caef4e693cbe0073c48c
Ryan Helmn <ryan.helmn@indiespring.com>
2023-01-11 15:00:26 +0000

    Added loading indicator and error message

--- src/App.tsx
+++ src/App.tsx
@@ -2,109 +2,127 @@ import React, { useState } from "react";
 import { createDetailsWidget } from "@livechat/agent-app-sdk";
 import PlaneImage from "./images/plane.svg";
 import WheelchairImage from "./images/wheelchair.svg";
+import { Loader } from "./components/Loader";
 
 function App() {
-  const [profile, setProfile] = useState({});
+  const [profile, setProfile] = useState<{}>();
+  const [error, setError] = useState<string>();
 
-  createDetailsWidget().then((widget) => {
-    widget.on("customer_profile", (profile) => {
-      if (profile.source !== "chats") {
-        return <h1>Can't fetch booking information outside of chat...</h1>;
-      }
+  createDetailsWidget()
+    .then((widget) => {
+      widget.on("customer_profile", (profile) => {
+        if (profile.source !== "chats") {
+          return <h1>Can't fetch booking information outside of chat...</h1>;
+        }
 
-      setProfile(profile);
+        setProfile(profile);
+      });
+    })
+    .catch(() => {
+      setError("Unable to load booking information");
     });
-  });
 
   return (
     <div className="flex flex-col gap-y-4 justify-center items-center py-10 px-2">
-      <div className="max-w-[343px] w-full">
-        <div className="bg-black w-full rounded-tl-[20px] rounded-tr-[20px] h-5"></div>
-        <div className="bg-light-grey py-6 px-4 flex flex-col gap-y-6 rounded-bl-[20px] rounded-br-[20px]">
-          <div className="flex justify-between items-center">
-            <span className="uppercase">Departure</span>
-            <span className="uppercase">Arrival</span>
-          </div>
+      {!profile && !error ? (
+        <Loader />
+      ) : error ? (
+        <span className="text-xl">{error}</span>
+      ) : (
+        <>
+          {" "}
+          <div className="max-w-[343px] w-full">
+            <div className="bg-black w-full rounded-tl-[20px] rounded-tr-[20px] h-5"></div>
+            <div className="bg-light-grey py-6 px-4 flex flex-col gap-y-6 rounded-bl-[20px] rounded-br-[20px]">
+              <div className="flex justify-between items-center">
+                <span className="uppercase">Departure</span>
+                <span className="uppercase">Arrival</span>
+              </div>
 
-          <div className="flex justify-between items-center relative">
-            <div>
-              <span className="uppercase text-2xl">LHR</span>
-              <span className="uppercase opacity-30">London</span>
-            </div>
+              <div className="flex justify-between items-center relative">
+                <div>
+                  <span className="uppercase text-2xl">LHR</span>
+                  <span className="uppercase opacity-30">London</span>
+                </div>
 
-            <div className="absolute flex justify-center w-full">
-              <img alt="Plane" src={PlaneImage} className="" />
-            </div>
+                <div className="absolute flex justify-center w-full">
+                  <img alt="Plane" src={PlaneImage} className="" />
+                </div>
 
-            <div>
-              <span className="uppercase text-2xl text-right">PAR</span>
-              <span className="uppercase opacity-30 text-right">Paris</span>
-            </div>
-          </div>
+                <div>
+                  <span className="uppercase text-2xl text-right">PAR</span>
+                  <span className="uppercase opacity-30 text-right">Paris</span>
+                </div>
+              </div>
 
-          <div>
-            <span className="uppercase text-xs">Departs At</span>
-            <span className="uppercase text-lg">09:00</span>
-            <span className="uppercase opacity-30">20/08/2021</span>
-          </div>
+              <div>
+                <span className="uppercase text-xs">Departs At</span>
+                <span className="uppercase text-lg">09:00</span>
+                <span className="uppercase opacity-30">20/08/2021</span>
+              </div>
 
-          <div className="flex items-center gap-x-2 overflow-x-auto pb-3">
-            <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
-              <span className="uppercase text-sm text-white">2 Pax</span>
-            </div>
+              <div className="flex items-center gap-x-2 overflow-x-auto pb-3">
+                <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
+                  <span className="uppercase text-sm text-white">2 Pax</span>
+                </div>
 
-            <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
-              <img src={WheelchairImage} alt="Wheelchair" />
-              <span className="uppercase text-sm text-white">Step-Free</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="max-w-[343px] w-full">
-        <div className="bg-black w-full rounded-tl-[20px] rounded-tr-[20px] h-5"></div>
-        <div className="bg-light-grey py-6 px-4 flex flex-col gap-y-6 rounded-bl-[20px] rounded-br-[20px]">
-          <div className="flex justify-between items-center">
-            <span className="uppercase">Departure</span>
-            <span className="uppercase">Arrival</span>
-          </div>
-
-          <div className="flex justify-between items-center relative">
-            <div>
-              <span className="uppercase text-2xl">PAR</span>
-              <span className="uppercase opacity-30">Paris</span>
-            </div>
-
-            <div className="absolute flex justify-center w-full">
-              <img alt="Plane" src={PlaneImage} className="" />
-            </div>
-
-            <div>
-              <span className="uppercase text-2xl text-right">LHR</span>
-              <span className="uppercase opacity-30 text-right">London</span>
+                <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
+                  <img src={WheelchairImage} alt="Wheelchair" />
+                  <span className="uppercase text-sm text-white">
+                    Step-Free
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
+          <div className="max-w-[343px] w-full">
+            <div className="bg-black w-full rounded-tl-[20px] rounded-tr-[20px] h-5"></div>
+            <div className="bg-light-grey py-6 px-4 flex flex-col gap-y-6 rounded-bl-[20px] rounded-br-[20px]">
+              <div className="flex justify-between items-center">
+                <span className="uppercase">Departure</span>
+                <span className="uppercase">Arrival</span>
+              </div>
 
-          <div>
-            <span className="uppercase text-xs">Departs At</span>
-            <span className="uppercase text-lg">12:00</span>
-            <span className="uppercase opacity-30">27/08/2021</span>
-          </div>
+              <div className="flex justify-between items-center relative">
+                <div>
+                  <span className="uppercase text-2xl">PAR</span>
+                  <span className="uppercase opacity-30">Paris</span>
+                </div>
 
-          <div className="flex items-center gap-x-2 overflow-x-auto pb-3">
-            <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
-              <span className="uppercase text-sm text-white">2 Pax</span>
+                <div className="absolute flex justify-center w-full">
+                  <img alt="Plane" src={PlaneImage} className="" />
+                </div>
+
+                <div>
+                  <span className="uppercase text-2xl text-right">LHR</span>
+                  <span className="uppercase opacity-30 text-right">
+                    London
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <span className="uppercase text-xs">Departs At</span>
+                <span className="uppercase text-lg">12:00</span>
+                <span className="uppercase opacity-30">27/08/2021</span>
+              </div>
+
+              <div className="flex items-center gap-x-2 overflow-x-auto pb-3">
+                <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
+                  <span className="uppercase text-sm text-white">2 Pax</span>
+                </div>
+
+                <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
+                  <img src={WheelchairImage} alt="Wheelchair" />
+                  <span className="uppercase text-sm text-white">
+                    Step-Free
+                  </span>
+                </div>
+              </div>
             </div>
-
-            <div className="bg-black px-2.5 py-1.5 rounded-[69px] flex-shrink-0 flex items-center gap-x-2">
-              <img src={WheelchairImage} alt="Wheelchair" />
-              <span className="uppercase text-sm text-white">Step-Free</span>
-            </div>
           </div>
-        </div>
-      </div>
-
-      {JSON.stringify(profile)}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Loader() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
